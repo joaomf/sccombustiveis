4c6bfe50a2ac964942685d5f2cf6ce406c21f473
Anderson Pereira de Lima <944931+andersonplima@users.noreply.github.com>
2019-10-19 14:25:40 -0300

    Criada classe de gastos com calculo de autonomia

--- SCCombustiveis/src/aulas/Gastos.java
+++ SCCombustiveis/src/aulas/Gastos.java
@@ -1,5 +1,48 @@
 package aulas;
 
 public class Gastos {
+    private String nome;
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
 
+    private float volume;
+    /**
+     * @return the volume
+     */
+    public float getVolume() {
+        return volume;
+    }
+
+    private float distanciaPercorrida;
+    /**
+     * @return the distanciaPercorrida
+     */
+    public float getDistanciaPercorrida() {
+        return distanciaPercorrida;
+    }
+
+    private float autonomia;
+    /**
+     * @return the autonomia
+     */
+    public float getAutonomia() {
+        return autonomia;
+    }
+    
+    public Gastos(String nome, float volume, float distanciaPercorrida) {
+        super();
+        this.nome = nome;
+        this.volume = volume;
+        this.distanciaPercorrida = distanciaPercorrida;
+        autonomia = new RegrasNegocio().calcularAutonomia(distanciaPercorrida, volume);
+    }   
+    
+    @Override
+    public String toString() {
+        return String.format("{0} {1} {2} {3}", nome, distanciaPercorrida, volume, autonomia);
+    }
 }
