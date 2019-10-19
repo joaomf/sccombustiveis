af05520cc7bf6a7b2e5cda40840dd1155e537f39
edils <edils@10.10.154.116>
2019-10-19 14:23:08 -0300

    Criada a função de cálculo de autonomia.

--- SCCombustiveis/src/aulas/RegrasNegocio.java
+++ SCCombustiveis/src/aulas/RegrasNegocio.java
@@ -1,5 +1,11 @@
 package aulas;
 
 public class RegrasNegocio {
-
+	
+	public float  calcularAutonomia(float kmrodado, float litros) {
+	
+	float autonomia = kmrodado/litros;
+	
+	return autonomia;
+	}
 }
